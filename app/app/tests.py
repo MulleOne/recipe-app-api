68a0dd4c54bec4274df0b599b5be0701176e100c
Zeeng <sir.zeeng@gmail.com>
2025-01-30 14:12:47 +0200

    Removed the __init__.py in the root directory which was causing the - '''ModuleNotFoundError: No module named 'app.app'.'''' error.

--- app/app/tests.py
+++ app/app/tests.py
@@ -9,10 +9,10 @@ class CalcTests(SimpleTestCase):
     """Test the calc module."""
     def test_add_numbers(self):
         """Test that two numbers are added together."""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
         """Test that two numbers are subtracted."""
-        res = calc.subtract(10, 15)
-        self.assertEqual(res, 5)
+        res = calc.subtract(6, 1)
+        self.assertEqual(res, -5)
